23c2971b34c303e99abc51f0cd885018faea6335
FZoltan40 <farkasz@kkszki.hu>
2025-12-06 09:51:53 +0100

    Models and dBcontext are created with Scaffold command.

---
+++ VedeglatasApi/Controllers/VarosokController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VedeglatasApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VarosokController : ControllerBase
+    {
+    }
+}
---
+++ VedeglatasApi/Models/Furdok.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VedeglatasApi.Models;
+
+public partial class Furdok
+{
+    public int Id { get; set; }
+
+    public string? Nev { get; set; }
+
+    public string? Cim { get; set; }
+
+    public string? Irnyitoszam { get; set; }
+
+    public DateTime? Regtime { get; set; }
+
+    public int? Varosid { get; set; }
+
+    public virtual Varosok? Varos { get; set; }
+}
